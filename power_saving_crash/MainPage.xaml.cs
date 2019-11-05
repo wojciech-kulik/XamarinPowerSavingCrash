73f1a207bf613f0217e64de053907289692ffb17
Wojciech Kulik <wkulik91@gmail.com>
2019-11-05 15:23:38 +0100

    Initial commit

---
+++ power_saving_crash/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace power_saving_crash
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private int counter = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.RotateElement(this.icon);
+        }
+
+        private async Task RotateElement(VisualElement element)
+        {
+            while (true)
+            {
+                await element.RotateTo(360, 1200);
+				element.Rotation = 0;
+
+                // workaround: await Task.Delay(1);
+
+                // also happens for other animations:
+                //await element.FadeTo(0, 1200, Easing.Linear);
+                //await element.FadeTo(1, 10, Easing.Linear);
+            }
+        }
+
+        public void ButtonClicked(object sender, EventArgs e)
+        {
+            this.testLabel.Text = (counter++).ToString();
+        }
+    }
+}
